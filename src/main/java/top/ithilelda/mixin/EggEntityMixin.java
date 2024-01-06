bbd8aad92e82c2add724d1c0089cc2800368fe18
ithilelda <ithilelda@gmail.com>
2024-01-06 09:29:12 +0800

    backport 1.1.1's bugfix.
    (cherry picked from commit 91c2d7d5b37570d186b59d07ff2d895b3b8ac845)

--- src/main/java/top/ithilelda/mixin/EggEntityMixin.java
+++ src/main/java/top/ithilelda/mixin/EggEntityMixin.java
@@ -17,46 +17,53 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import top.ithilelda.Egginator;
 
-import java.util.Optional;
-
 @Mixin(EggEntity.class)
 public abstract class EggEntityMixin {
 
-	@Inject(at = @At("HEAD"), method = "onEntityHit")
-	private void getHit(EntityHitResult entityHitResult, CallbackInfo ci) {
-		EggEntity entity = (EggEntity)(Object)this;
-		if (entityHitResult.getEntity() instanceof LivingEntity le) {
-			if (!spawnEntityEgg(le)) {
-				Egginator.LOGGER.warn("The egg for entity " + le.getName() + " has failed to drop!");
-			}
-		}
-	}
+    @Inject(at = @At("HEAD"), method = "onEntityHit")
+    private void spawnEntityEggOnLivingEntityHit(EntityHitResult entityHitResult, CallbackInfo ci) {
+        if (entityHitResult.getEntity() instanceof LivingEntity le && Egginator.canCreateSpawnEgg(le)) {
+            if (!spawnEntityEgg(le)) {
+                Egginator.LOGGER.warn("The egg for entity " + le.getName() + " has failed to drop!");
+            }
+        }
+    }
 
-	@ModifyExpressionValue(at = @At(value = "FIELD", target = "net/minecraft/world/World.isClient:Z"), method = "onCollision")
-	private boolean cancelChickenSpawnOnLivingEntityHit(boolean original, HitResult hitResult) {
-		return original || (hitResult.getType() == HitResult.Type.ENTITY && ((EntityHitResult) hitResult).getEntity() instanceof LivingEntity);
-	}
+    @ModifyExpressionValue(at = @At(value = "INVOKE", target = "net/minecraft/util/math/random/Random.nextInt(I)I", ordinal = 0),
+            method = "onCollision")
+    private int cancelChickSpawnOnLivingEntityHit(int original, HitResult hitResult) {
+        // if we are hitting a living entity, we never spawn an egg (a none zero value will do).
+        if ((hitResult.getType() == HitResult.Type.ENTITY && ((EntityHitResult) hitResult).getEntity() instanceof LivingEntity)) {
+            return 1;
+        }
+        else {
+            return original;
+        }
+    }
 
-	@Unique
-	private boolean spawnEntityEgg(LivingEntity entity) {
-		SpawnEggItem spawnEgg = SpawnEggItem.forEntity(entity.getType());
-		if (spawnEgg != null) {
-			Egginator.LOGGER.debug("Spawn Egg of Entity " + EntityType.getId(entity.getType()) + " found!");
-			NbtCompound entityNbt = new NbtCompound();
-			entity.writeNbt(entityNbt);
-			Egginator.LOGGER.debug(entityNbt.toString());
-			entityNbt.remove("Pos");
-			entityNbt.remove("UUID");
-			NbtCompound itemNbt = new NbtCompound();
-			itemNbt.put(EntityType.ENTITY_TAG_KEY, entityNbt);
-			ItemStack spawnEggStack = new ItemStack(spawnEgg);
-			spawnEggStack.setNbt(itemNbt);
-			ItemEntity ie = entity.dropStack(spawnEggStack);
-			if (ie != null) {
-				entity.discard();
-				return true;
-			}
-		}
-		return false;
-	}
+    @Unique
+    private boolean spawnEntityEgg(LivingEntity entity) {
+        SpawnEggItem spawnEgg = SpawnEggItem.forEntity(entity.getType());
+        if (spawnEgg != null) {
+            Egginator.LOGGER.debug("Spawn Egg of Entity " + EntityType.getId(entity.getType()) + " found!");
+            NbtCompound entityNbt = new NbtCompound();
+            entity.writeNbt(entityNbt);
+            Egginator.LOGGER.debug(entityNbt.toString());
+            entityNbt.remove("Pos");
+            entityNbt.remove("UUID");
+            entityNbt.remove("Motion");
+            entityNbt.remove("Rotation");
+            entityNbt.remove("OnGround");
+            NbtCompound itemNbt = new NbtCompound();
+            itemNbt.put(EntityType.ENTITY_TAG_KEY, entityNbt);
+            ItemStack spawnEggStack = new ItemStack(spawnEgg);
+            spawnEggStack.setNbt(itemNbt);
+            ItemEntity ie = entity.dropStack(spawnEggStack);
+            if (ie != null) {
+                entity.discard();
+                return true;
+            }
+        }
+        return false;
+    }
 }
