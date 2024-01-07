ed2f80c0a43731215804222e2f778a40dd1e09c1
ithilelda <ithilelda@gmail.com>
2024-01-07 09:01:17 +0800

    change mixin hook point so that intellij doesn't crash.

--- src/main/java/top/ithilelda/mixin/MobSpawnerLogicMixin.java
+++ src/main/java/top/ithilelda/mixin/MobSpawnerLogicMixin.java
@@ -1,30 +1,17 @@
 package top.ithilelda.mixin;
 
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
-import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
-import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
-import net.minecraft.entity.Entity;
-import net.minecraft.server.world.ServerWorld;
 import net.minecraft.world.MobSpawnerLogic;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import top.ithilelda.Egginator;
 
 @Mixin(MobSpawnerLogic.class)
 public class MobSpawnerLogicMixin {
-    @ModifyExpressionValue(at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;isPlayerInRange(DDDD)Z"), method = "isPlayerInRange")
+    @ModifyExpressionValue(at = @At(value = "INVOKE",
+                target = "net/minecraft/world/MobSpawnerLogic.isPlayerInRange (Lnet/minecraft/world/World;Lnet/minecraft/util/math/BlockPos;)Z"),
+            method = "serverTick")
     private boolean interceptPlayerCheck(boolean original) {
         if (((MobSpawnerLogicAccessor)this).getRequiredPlayerRange() < 0) return true;
         else return original;
     }
-/*
-    @WrapOperation(at = @At(value = "INVOKE", target = "net/minecraft/server/world/ServerWorld.spawnNewEntityAndPassengers(Lnet/minecraft/entity/Entity;)Z"), method = "serverTick")
-    private boolean debug(ServerWorld instance, Entity entity, Operation<Boolean> original) {
-        boolean result = original.call(instance, entity);
-        if (result) {
-            Egginator.LOGGER.info("successfully spawned mob " + entity.getType());
-        }
-        return result;
-    }
- */
 }
